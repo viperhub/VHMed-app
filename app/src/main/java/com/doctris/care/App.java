865bfe0cc2124538c0f72a3e9f782267e5498ed0
Khương Hùng <khuonghung1423@gmail.com>
2022-10-04 13:12:25 +0700

    storage: add support SharedPreferences

---
+++ app/src/main/java/com/doctris/care/App.java
@@ -0,0 +1,17 @@
+package com.doctris.care;
+
+import android.app.Application;
+
+public class App extends Application {
+    private static App mSelf;
+
+    public static App self() {
+        return mSelf;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mSelf = this;
+    }
+}
---
+++ app/src/main/java/com/doctris/care/storage/SharedPrefManager.java
@@ -0,0 +1,59 @@
+package com.doctris.care.storage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.doctris.care.App;
+
+public class SharedPrefManager {
+    private static final String PREFS_NAME = "share_prefs";
+    private static SharedPrefManager mInstance;
+    private final SharedPreferences mSharedPreferences;
+
+    private SharedPrefManager() {
+        mSharedPreferences = App.self().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static SharedPrefManager getInstance() {
+        if (mInstance == null) {
+            mInstance = new SharedPrefManager();
+        }
+        return mInstance;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(String key, Class<T> anonymousClass) {
+        if (anonymousClass == String.class) {
+            return (T) mSharedPreferences.getString(key, "");
+        } else if (anonymousClass == Boolean.class) {
+            return (T) Boolean.valueOf(mSharedPreferences.getBoolean(key, false));
+        } else if (anonymousClass == Float.class) {
+            return (T) Float.valueOf(mSharedPreferences.getFloat(key, 0));
+        } else if (anonymousClass == Integer.class) {
+            return (T) Integer.valueOf(mSharedPreferences.getInt(key, 0));
+        } else if (anonymousClass == Long.class) {
+            return (T) Long.valueOf(mSharedPreferences.getLong(key, 0));
+        }
+        return null;
+    }
+
+    public <T> void put(String key, T data) {
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        if (data instanceof String) {
+            editor.putString(key, (String) data);
+        } else if (data instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) data);
+        } else if (data instanceof Float) {
+            editor.putFloat(key, (Float) data);
+        } else if (data instanceof Integer) {
+            editor.putInt(key, (Integer) data);
+        } else if (data instanceof Long) {
+            editor.putLong(key, (Long) data);
+        }
+        editor.apply();
+    }
+
+    public void clear() {
+        mSharedPreferences.edit().clear().apply();
+    }
+}
