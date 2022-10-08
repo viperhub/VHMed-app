bfc724d748c5493a328e474e29958a6eed8fd8bc
Khương Hùng <khuonghung1423@gmail.com>
2022-10-08 14:11:50 +0700

    ui: update design for Bottom Navigation & Action Bar

--- app/src/main/java/com/doctris/care/ui/activity/HomeActivity.java
+++ app/src/main/java/com/doctris/care/ui/activity/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.doctris.care.ui.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +11,24 @@ import com.doctris.care.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bindingView();
+        setSupportActionBar(toolbar);
     }
+
+    private void bindingView() {
+        toolbar = findViewById(R.id.action_bar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
